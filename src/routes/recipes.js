a1cae775739f1e21572dd90eb80e2700a313f2f6
shriniket <shriniketkulkarni42@gmail.com>
2023-07-29 19:10:28 +0530

    first commit

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,70 +1,88 @@
-import express from 'express'
+import express from "express";
 import { Recipemodel } from "../models/Recipes.js";
-import { Usermodel } from '../models/Users.js';
-import { verifyToken } from './users.js';
+import { Usermodel } from "../models/Users.js";
+import { verifyToken } from "./users.js";
 
 const router = express.Router();
 
-
 router.get("/", async (req, res) => {
-    try {
-        const response = await Recipemodel.find({});
-        res.json(response);
-    } catch (err) {
-        res.json(err)
-    }
-})
+  try {
+    const response = await Recipemodel.find({});
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post("/", verifyToken, async (req, res) => {
-    const recipe = new Recipemodel(req.body);
-    try {
-        const response = await recipe.save();
-        res.json(response);
-    } catch (err) {
-        res.json(err)
-    }
-})
+  const recipe = new Recipemodel({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    image: req.body.image,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    imageUrl: req.body.imageUrl,
+    cookingTime: req.body.cookingTime,
+    userOwner: req.body.userOwner,
+  });
+  console.log(recipe);
+  try {
+    const response = await recipe.save();
+    res.status(201).json({
+      createdRecipe: {
+        name: result.name,
+        image: result.image,
+        ingredients: result.ingredients,
+        instructions: result.instructions,
+        _id: result._id,
+      },
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+// Get a recipe by ID
+router.get("/:recipeId", async (req, res) => {
+  try {
+    const result = await Recipemodel.findById(req.params.recipeId);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.put("/", verifyToken, async (req, res) => {
-    
-    try {
-        const recipe = await Recipemodel.findById(req.body.recipeID);
-        const user = await Usermodel.findById(req.body.userID);
-
-        user.savedRecipes.push(recipe);
-        await user.save();
-        res.json({savedRecipes: user.savedRecipes});
-    } catch (err) {
-        res.json(err)
-    }
+router.put("/", async (req, res) => {
+  const recipe = await Recipemodel.findById(req.body.recipeID);
+  const user = await Usermodel.findById(req.body.userID);
+  try {
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.status(201).json({ savedRecipes: user.savedRecipes });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/savedRecipes/ids/:userID", async (req, res) => {
-    try {
-        const user = await Usermodel.findById(req.params.userID)
-        res.json({savedRecipes: user?.savedRecipes})
-    } catch (err) {
-        res.json(err);
-    }
+  try {
+    const user = await Usermodel.findById(req.params.userID);
+    res.status(201).json({ savedRecipes: user?.savedRecipes });
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 router.get("/savedRecipes/:userID", async (req, res) => {
-    try {
-        const user = await Usermodel.findById(req.params.userID);
-        const savedRecipes = await Recipemodel.find({
-            _id: { $in: user.savedRecipes }
-        });
-        res.json({savedRecipes});
-    } catch (err) {
-        res.json(err);
-    }
+  try {
+    const user = await Usermodel.findById(req.params.userID);
+    const savedRecipes = await Recipemodel.find({
+      _id: { $in: user.savedRecipes },
+    });
+    res.status(201).json({ savedRecipes });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-
-
-export { router as recipesRouter }
-
-
-
-
+export { router as recipesRouter };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,76 +1,69 @@
 import express from "express";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
 import { Usermodel } from "../models/Users.js";
 
-router.post("/register",async (req, res) => {
-    const { username, password } = req.body;
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
 
-    const user = await Usermodel.findOne({username})  //checking in db whether the name enterred here matches in the db
+  const user = await Usermodel.findOne({ username }); //checking in db whether the name enterred here matches in the db
 
-    // if username matches that entered in front as in db
-    if(user) {
-        return res.json({message: "User already exists"});
-    }
+  // if username matches that entered in front as in db
+  if (user) {
+    return res.status(400).json({ message: "User already exists" });
+  }
 
-    //if there is no such usename we come here and hash the password
-    const hashedPassword = await bcrypt.hash(password, 10)
+  //if there is no such usename we come here and hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = new Usermodel({
-        username,
-         password: hashedPassword
-        });
+  const newUser = new Usermodel({
+    username,
+    password: hashedPassword,
+  });
 
-        await newUser.save();
+  await newUser.save();
 
-    res.json({message: "User registered successfully"});
-
+  res.json({ message: "User registered successfully" });
 });
 
 router.post("/login", async (req, res) => {
-    const {username, password} = req.body;
-    const user = await Usermodel.findOne({username});
+  const { username, password } = req.body;
+  const user = await Usermodel.findOne({ username });
 
-    //if user is not found in db
-    if(!user) {
-        return res.json({message: "User Does'nt exists!"})
-    }
+  //if user is not found in db
+  if (!user) {
+    return res.status(400).json({ message: "User Does'nt exists!" });
+  }
 
-    //here we compare the password by again encrypting the one entered with the one in db
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+  //here we compare the password by again encrypting the one entered with the one in db
+  const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordValid) {
-        return res.json({message: "Username or password is Incorrect"});
-    }
+  if (!isPasswordValid) {
+    return res
+      .status(400)
+      .json({ message: "Username or password is Incorrect" });
+  }
 
-    //for authentication session creation
-    const token = jwt.sign({id: user._id}, process.env.TOKEN);
+  //for authentication session creation
+  const token = jwt.sign({ id: user._id }, process.env.TOKEN);
 
-    res.json({token, userID: user._id});
+  res.json({ token, userID: user._id });
+});
 
-})
-
-
-
-
-
-
-
-
-export {router as userRouter};
+export { router as userRouter };
 
 //middleware
 export const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization;
-    if (token) {
-        jwt.verify(token, "secret", (err) => {
-            if(err) return res.sendStatus(403);
-            next();
-        });
-    } else {
-        res.sendStatus(401);
-    }
-}
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, process.env.TOKEN, (err) => {
+      if (err) return res.sendStatus(403);
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
